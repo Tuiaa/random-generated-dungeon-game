9682172eab1749e7ae71618705817155960dd2d3
minjasilven <minja.silvennoinen@futurice.com>
2020-02-09 16:46:06 +0200

    Fixed gitignore & updated the project

---
+++ Assets/Scripts/DungeonGenerator/RoomGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenerator {
+    
+    private Room _roomToGenerate;
+
+	public void CreateRoom(Room room)
+    {
+        _roomToGenerate = room;
+
+        if(_roomToGenerate != null)
+        {
+            CreateFloor(_roomToGenerate._width, _roomToGenerate._height);
+            CreateWalls();
+        }
+    }
+
+    private void CreateFloor(int width, int height)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject _floorQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                _floorQuad.transform.position = new Vector3(_roomToGenerate._position.x + i, 0, _roomToGenerate._position.y + j);
+                _floorQuad.transform.Rotate(90, 0, 0, Space.World);
+            }
+        }
+    }
+
+    private void CreateWalls()
+    {
+
+    }
+
+    private void PositionCamera()
+    {
+
+    }
+}
